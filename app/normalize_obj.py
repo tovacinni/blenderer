aaf541bf03eeea856144a174d8b38b65525cddb4
Towaki Takikawa <tovacinni@gmail.com>
2021-04-26 20:06:54 -0400

    Normalize to sphere

--- app/normalize_obj.py
+++ app/normalize_obj.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from mathutils import Vector
+import math
 
 import bpy
 
@@ -17,7 +18,6 @@ bpy.ops.import_scene.obj(filepath=meshPath, split_mode='OFF')
 
 # From: https://blender.stackexchange.com/questions/58029/how-to-make-an-object-fit-in-a-unit-cube
 
-
 #obj = bpy.context.object
 
 #Eventually apply transforms (comment if unwanted)
@@ -30,19 +30,39 @@ minX = min( [vertex.co[0] for vertex in ctx['active_object'].data.vertices] )
 minY = min( [vertex.co[1] for vertex in ctx['active_object'].data.vertices] )
 minZ = min( [vertex.co[2] for vertex in ctx['active_object'].data.vertices] )
 
-vMin = Vector( [minX, minY, minZ] )
+maxX = max( [vertex.co[0] for vertex in ctx['active_object'].data.vertices] )
+maxY = max( [vertex.co[1] for vertex in ctx['active_object'].data.vertices] )
+maxZ = max( [vertex.co[2] for vertex in ctx['active_object'].data.vertices] )
 
-maxDim = max(ctx['active_object'].dimensions)
+centerX = (minX + maxX) / 2.0
+centerY = (minY + maxY) / 2.0
+centerZ = (minZ + maxZ) / 2.0
 
-if maxDim != 0:
-    for v in ctx['active_object'].data.vertices:
-        v.co -= vMin #Set all coordinates start from (0, 0, 0)
-        v.co /= maxDim #Set all coordinates between 0 and 1
-        v.co *= Vector( [2.0, 2.0, 2.0] )
-        v.co -= Vector( [1.0, 1.0, 1.0] )
-else:
-    for v in ctx['active_object'].data.vertices:
-        v.co -= vMin
+centerV = Vector( [centerX, centerY, centerZ] )
+
+dists = []
+
+# New code: Fit in unit sphere
+for v in ctx['active_object'].data.vertices:
+    v.co -= centerV
+    dists.append(math.sqrt(v.co.x**2 + v.co.y**2 + v.co.z**2))
+max_dist = max(dists)
+for v in ctx['active_object'].data.vertices:
+    v.co /= max_dist
+
+# Old code: Fit in Unit Cube
+
+#vMin = Vector( [minX, minY, minZ] )
+#maxDim = max(ctx['active_object'].dimensions)
+#if maxDim != 0:
+#    for v in ctx['active_object'].data.vertices:
+#        v.co -= vMin #Set all coordinates start from (0, 0, 0)
+#        v.co /= maxDim #Set all coordinates between 0 and 1
+#        v.co *= Vector( [2.0, 2.0, 2.0] )
+#        v.co -= Vector( [1.0, 1.0, 1.0] )
+#else:
+#    for v in ctx['active_object'].data.vertices:
+#        v.co -= vMin
 
 bpy.ops.export_scene.obj(filepath=outPath)
 
